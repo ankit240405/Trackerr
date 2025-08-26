05b6f4d38f8476f450ad4641cf4c029a5cecd8cc
ankit240405 <ankitugnsut@gmail.com>
2025-08-27 02:09:30 +0530

    few changes made

--- app.js
+++ app.js
@@ -27,4 +27,5 @@ io.on("connection", (socket) => {
 app.get("/",function(req,res){
     res.render("index");
 });
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
